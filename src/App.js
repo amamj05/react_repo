a2cbf33dd7ea578c2ef6e859b42c29d8103df483
mj <ui2939@gmail.com>
2024-05-31 16:24:22 +0900

    func Header / Nav / Body

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h2><a href="/">MAIN</a></h2>
+    </header>)
+}
+
+function Nav() {
+  return (
+    <nav><ol>
+      <li><a href="#">html</a></li>
+      <li><a href="#">css</a></li>
+      <li><a href="#">js</a></li>
+    </ol></nav>)
+}
+
+function Body() {
+  return (
+    <body>hello<br></br>hi</body>)
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>시작합니다</p>
+
+      <Header></Header>
+      <Nav></Nav>
+      <Body></Body>
+
     </div>
   );
 }
+
+
 
 export default App;
