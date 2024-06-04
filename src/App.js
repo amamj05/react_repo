7603caab73081ce2829e22dc40e28064665b8363
mj <ui2939@gmail.com>
2024-06-04 15:26:51 +0900

    add Create component

--- src/App.js
+++ src/App.js
@@ -42,33 +42,64 @@ function Nav(props) {
 }
 
 
+function Create(props){
+  return(
+  <article>
+    <h2>Create</h2>
+    <form onSubmit={(e)=>{e.preventDefault();
+      const title = e.target.title.value;
+      const body = e.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" placeholder='title' name="title"/></p>
+      <p><textarea placeholder='body' name="body"></textarea></p>
+      <input type="submit" value="Create"/>
+    </form>
+  </article>
+  )
+}
+
+
+
 function App() {
 
-const [mode, setMode] = useState('Welcome');
-const [ _id, set_id] = useState(null);
-  // let mode = 'Welcome';
-  const topic = [
+  const [mode, setMode] = useState('Welcome');
+  const [_id, set_id] = useState(null);
+  const [topic, setTopic] = useState([
     { id: 1, title: "apple", body: "apple is ..." },
     { id: 2, title: "lemon", body: "lemon is ..." },
-    { id: 3, title: "melon", body: "melon is ..." }];
-
+    { id: 3, title: "melon", body: "melon is ..." }]
+  );
+  const [newId, setNewId] = useState(4);
+  // let mode = 'Welcome';
+  
 
   let content = null;
 
-  if ( mode === 'Welcome') {
+  if (mode === 'Welcome') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
-  } else if ( mode === 'READ') {
+  } else if (mode === 'READ') {
     let title, body = null;
-    
-    // set_id(iidd);
-    console.log(_id);
-    for( let i=0; i<topic.length; i++){
-      if(topic[i].id === _id){
+
+    // console.log(_id);
+    for (let i = 0; i < topic.length; i++) {
+      if (topic[i].id === _id) {
         title = topic[i].title;
         body = topic[i].body;
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === 'Create'){
+    content = <Create onCreate={(_title, _body)=>{
+      console.log(newId, _title, _body);
+      const newTopic = {id:newId , title:_title, body:_body };
+      const newTopics = [...topic]
+      setNewId(newId+1);
+      newTopics.push(newTopic);
+      setTopic(newTopics);
+      setMode('READ');
+      set_id(newId);
+    }}></Create>
   }
 
 
@@ -86,6 +117,8 @@ const [ _id, set_id] = useState(null);
         set_id(iidd);
       }}></Nav>
       {content}
+      <a href="/create" onClick={(e)=>{e.preventDefault(); setMode('Create'); }}
+      >Create</a>
 
     </div>
   );
