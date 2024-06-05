bb5f18bc9ba797a1044015624c26a88b654d018f
mj <ui2939@gmail.com>
2024-06-05 18:23:11 +0900

    add welcome.jpg

--- src/App.practice.js
+++ src/App.practice.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import welcome from './img/welcome.jpg';
 
 function Nav(){
     return(
         <article>
-            <ul>
+            <ul className='no-dot'>
                 <li><a href='/'>HOME</a></li>
                 <li><a href='/create' onClick={(e)=>{e.preventDefault();}}>글쓰기</a></li>
             </ul>
+            <hr></hr>
         </article>
+    )
+}
+
+function Headers(){
+    return(
+        <><h1>~~WELCOME~~</h1>
+        <img src={welcome} width="500px"></img>
+        </>
+
     )
 }
 
@@ -16,11 +27,11 @@ function Create(){
     return (
         <article>
         <h2>게시글 작성</h2>
-        <from>
+        <form>
             <p><input type="text" name="title" placeholder='제목'/></p>
             <p><textarea name="body" placeholder='내용을 적어주세요'></textarea></p>
             <input type='submit' value='create'/>
-        </from>
+        </form>
         </article>
     )
 }
@@ -29,7 +40,8 @@ function Create(){
 function App() {
   return (
     <div className="App">
-        <h2>리액트 CRUD 연습</h2>
+        <h4>리액트 CRUD 연습</h4>
+        <Headers></Headers>
         <Nav></Nav>
         <Create></Create>
     </div>
