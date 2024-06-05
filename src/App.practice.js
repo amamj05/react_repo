e8e0bc7ea3748199705183d4d51850aaa98050c2
mj <ui2939@gmail.com>
2024-06-05 17:28:52 +0900

    practice CRUD App.practice.js

--- src/App.practice.js
+++ src/App.practice.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Nav(){
+    return(
+        <article>
+            <ul>
+                <li><a href='/'>HOME</a></li>
+                <li><a href='/create' onClick={(e)=>{e.preventDefault();}}>글쓰기</a></li>
+            </ul>
+        </article>
+    )
+}
+
+function Create(){
+    return (
+        <article>
+        <h2>게시글 작성</h2>
+        <from>
+            <p><input type="text" name="title" placeholder='제목'/></p>
+            <p><textarea name="body" placeholder='내용을 적어주세요'></textarea></p>
+            <input type='submit' value='create'/>
+        </from>
+        </article>
+    )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>리액트 CRUD 연습</h2>
+        <Nav></Nav>
+        <Create></Create>
     </div>
   );
 }
