f7877a5165a146e2f2ff8e2bc7f6050bcfcb6866
mj <ui2939@gmail.com>
2024-06-10 17:27:08 +0900

    create Post List

--- src/App.practice.js
+++ src/App.practice.js
@@ -1,38 +1,56 @@
 import write from './img/write.jpg';
 import './App.css';
 import welcome from './img/welcome.jpg';
+import list from './img/list.png';
 import { useState } from 'react';
 
-function Nav(){
-    return(
+function Read(props) {
+
+    const _title = [];
+
+
+    for (let i = 0; i < props.post.length; i++) {
+        // let _title = props.post[i].title;
+        // let _body = props.post[i].body;
+
+        _title.push(<li key={props.post[i].id} className='post-li'>{props.post[i].title}</li>);
+
+        // postList.push({id:i, title:_title, body:_body});
+        console.log(_title);
+
+    }
+
+    return (
         <article>
-         
+            <img src={list} width="500px"></img>
+            <ol className='post-ol'>
+                {_title}
+            </ol>
         </article>
     )
 }
 
-function Headers(){
-    return(
-        <article>
-         
-        </article>
+function Headers() {
+    return (
+        <article></article>
     )
 }
 
-function Create(){
+function Create() {
     return (
         <article>
             <img src={write} width="500px"></img>
-        <h2>게시글 작성</h2>
-        <form onSubmit={(e)=>{e.preventDefault();
-            const title = e.target.title.value;
-            const body = e.target.body.value;
-            
-        }}>
-            <p><input type="text" name="title" placeholder='제목'/></p>
-            <p><textarea name="body" placeholder='내용을 적어주세요'></textarea></p>
-            <input type='submit' value='create'/>
-        </form>
+            <h2>게시글 작성</h2>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                const title = e.target.title.value;
+                const body = e.target.body.value;
+
+            }}>
+                <p><input type="text" name="title" placeholder='제목' /></p>
+                <p><textarea name="body" placeholder='내용을 적어주세요'></textarea></p>
+                <input type='submit' value='create' />
+            </form>
         </article>
     )
 }
@@ -42,34 +60,47 @@ function App() {
 
     const [mode, setMode] = useState('HOME');
     let middleContent = null;
-    if ( mode === 'HOME'){
+    let postList = [];
+    const post = [{ id: 1, title: 'title111', body: 'body111' },
+    { id: 2, title: 'title222', body: 'body222' },
+    { id: 3, title: 'title333', body: 'body333' }
+    ];
+
+    if (mode === 'HOME') {
         middleContent = <article>
-        <img src={welcome} width="500px"></img>
-        <h1>~~WELCOME~~</h1>
+            <img src={welcome} width="500px"></img>
+            <h1>~~WELCOME~~</h1>
         </article>
     }
-    else if(mode === 'Create'){
-        
+    else if (mode === 'Create') {
+
         middleContent = <Create></Create>;
     }
-  return (
-    <div className="App">
-        <h4>리액트 CRUD 연습</h4>
-        <Headers onChangeMode={()=>{setMode('HOME');}}></Headers>
-        <div>
-        <ul className='no-dot'>
-                <li><a href='/'>HOME</a></li>
-                <li><a href='/create' onClick={(e)=>{e.preventDefault();
-                    setMode('Create');
-                }}>글쓰기</a></li>
-            </ul>
-            {/* <hr></hr> */}
+    else if (mode === 'READ') {
+
+        middleContent = <Read post={post}></Read>;
+    }
+
+    return (
+        <div className='App'>
+            <h4>리액트 CRUD 연습</h4>
+            <Headers onChangeMode={() => { setMode('HOME'); }}></Headers>
+            <div>
+                <ul className='no-dot'>
+                    <li><a href='/'>HOME</a></li>
+                    <li><a href='/create' onClick={(e) => {
+                        e.preventDefault();
+                        setMode('Create');
+                    }}>글쓰기</a></li>
+                    <li><a href='/READ' onClick={(e) => { e.preventDefault(); setMode('READ'); }}>게시글 목록</a></li>
+                </ul>
+                {/* <hr></hr> */}
+            </div>
+
+            {middleContent}
+
         </div>
-        
-        {middleContent}
-        
-    </div>
-  );
+    );
 }
 
 export default App;
